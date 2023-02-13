50123295f546bea6f4a63b0e2d2494df52751440
Angeluuh <angelanicolebatoon@gmail.com>
2023-02-14 00:24:03 +0800

    contact page

---
+++ contact.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EC-Art</title>
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Font Awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS link -->
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="contact.css">
+    <!-- Google Fonts API link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        
+        <nav class="navbar fixed-top navbar-expand-lg header">
+            <div class="container-fluid px-5 pt-3">
+                <a href="#" class="navbar-brand me-5"><img src="./ec-logo.png" alt="EC-Art" class="logo"></a>
+                <form action="" class="d-none d-sm-inline">
+                    <div class="input-group search-bar">
+                        <input type="search" class="form-control border-0" placeholder="Search" aria-label="Search">
+                        <button class="btn search-btn border-0" type="submit">
+                        <i class="fas fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div> 
+                </form>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-semibold">
+                        <li class="nav-item px-4">
+                            <a class="nav-link" aria-current="page" href="ec_art.php">HOME</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">ABOUT</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="services.php">SERVICES</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link active-link" href="contact.php">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container p-0 top">
+            <div class="row">
+                <div class="col-lg-12 p-0">
+                    <div class="row mb-5">
+                        <span class="contact d-flex justify-content-center align-items-center ">Contact Us</span>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 p-0">
+                            <div class="mb-4"><img src="./contact-img1.jpg" alt="" class="contact-img p-0"></div>
+                            <div class="row h-50 fw-semibold m-0">
+                                <div class="d-flex flex-column justify-content-center">
+                                <p class="d-flex justify-content-end fw-semibold m-0 me-3">(02-7995-8901)</p>
+                                <p class="d-flex justify-content-end fw-semibold m-0 me-3">+63 906 046 1436</p>
+                                <p class="d-flex justify-content-end fw-semibold m-0 me-3">Available daily from 9:00 AM - 8:00 PM (except on holidays.)</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 p-0">
+                            <div class="row h-50 d-flex align-items-center fw-semibold p-0 m-0">
+                                <p class="p-0 m-0">
+                                Get assistance from our Customer Care Specialists, who are available 24/7, or speak to our 24/7 virtual customer service representative (except on holidays.)
+                                </p>
+                            </div>
+                            <div class="d-flex justify-content-end"><img src="./contact-img2.jpg" alt="" class="contact-img p-0"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container top">
+            <div class="d-flex justify-content-center">
+                <i class="fa-brands fa-instagram m-2 fs-1"></i>
+                <i class="fa-brands fa-facebook-f m-2 fs-1"></i>
+                <i class="fa-brands fa-twitter m-2 fs-1"></i>
+            </div>
+            <span class="d-flex justify-content-center fw-bold">@ecommerceart</span>
+        </div>
+    </div>
+
+
+    <!-- Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    
+</body>
+</html>
